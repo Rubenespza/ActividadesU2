6fee26d32b9b80e238ac022f0a1d90ed2080bbb3
i20050037 - GENARO RUBEN SAUCEDO ESPINOZA <70655396+Rubenespza@users.noreply.github.com>
2023-03-10 15:05:30 -0600

    Add files via upload

---
+++ Formulario2/config.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title></title>
+</head>
+<body>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "tec";
+?>
+</body>
+</html>
---
+++ Formulario2/eliminar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title></title>
+</head>
+<body>
+<?php
+    $borrar = $_POST["borrar"];
+        
+    $enlace = mysqli_connect("localhost", "root", "", "tec");
+    $datos = "DELETE FROM `calificaciones` WHERE IdCalificacion=".$borrar.";";
+    
+    if ($enlace->query($datos) == TRUE) {
+        $enlace->close();
+        header("Location: inicio.php");
+        exit();
+    } else {
+        $enlace->close();
+        header("Location: inicio.php");
+        exit();
+    }
+?>
+</body>
+</html>
